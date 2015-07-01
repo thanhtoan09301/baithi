75c985ea3d782daa2e5c944df8f21b5357362994
thanhtoan09301 <thanhtoan09301@yahoo.com>
2015-07-01 09:09:48 +0700

    up

---
+++ upload1.sql
@@ -0,0 +1,55 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-07-01 02:09:30.173
+
+
+
+
+-- tables
+-- Table: TNhanvien
+CREATE TABLE TNhanvien (
+    MaNV nchar(10)  NOT NULL,
+    TenNV nvarchar(50)  NOT NULL,
+    Ngaysinh date  NOT NULL,
+    GioiTinh nvarchar(5)  NOT NULL,
+    Email nvarchar(50)  NOT NULL,
+    MucLuong money  NOT NULL,
+    MaPhong nchar(10)  NOT NULL,
+    CONSTRAINT TNhanvien_pk PRIMARY KEY (MaNV)
+)
+;
+
+
+
+
+
+-- Table: TPhongban
+CREATE TABLE TPhongban (
+    MaPhong nchar(10)  NOT NULL,
+    Tenphong nvarchar(50)  NOT NULL,
+    CONSTRAINT TPhongban_pk PRIMARY KEY (MaPhong)
+)
+;
+
+
+
+
+
+
+
+
+
+-- foreign keys
+-- Reference:  TNhanvien_TPhongban (table: TNhanvien)
+
+
+ALTER TABLE TNhanvien ADD CONSTRAINT TNhanvien_TPhongban 
+    FOREIGN KEY (MaPhong)
+    REFERENCES TPhongban (MaPhong)
+;
+
+
+
+
+
+-- End of file.
+
